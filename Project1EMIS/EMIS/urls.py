420f7d603bd9f8ee847b264bddc5f0c188a6fed3
dagreenl <dgreenlees28@hotmail.com>
2016-11-30 19:35:55 -0600

    Removed unused files

--- Project1EMIS/EMIS/urls.py
+++
@@ -1,9 +0,0 @@
-from django.conf.urls import url
-from . import views
-
-urlpatterns = [
-    url(r'^$', views.index, name='index'),
-    url(r'^patient', views.patient, name='patient'),
-    url(r'^pat_pers-info/$', views.patPI, name='patPI'),
-    url(r'^pat_ins-info/$', views.patIns, name='patIns'),
-]
